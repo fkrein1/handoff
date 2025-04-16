92c8c565b00dff3459e77a659decf2f4c2082bbc
Felipe Krein Rocha <felipekrein@gmail.com>
2025-04-16 08:14:16 -0300

    feat: react compiler

---
+++ app.ts
@@ -0,0 +1,46 @@
+import { ExpoConfig } from 'expo/config';
+
+const config: ExpoConfig = {
+  name: 'frontend-take-home',
+  slug: 'frontend-take-home',
+  version: '1.0.0',
+  orientation: 'portrait',
+  icon: './assets/images/icon.png',
+  scheme: 'myapp',
+  userInterfaceStyle: 'automatic',
+  newArchEnabled: true,
+  ios: {
+    supportsTablet: true,
+  },
+  android: {
+    adaptiveIcon: {
+      foregroundImage: './assets/images/adaptive-icon.png',
+      backgroundColor: '#ffffff',
+    },
+  },
+  web: {
+    bundler: 'metro',
+    output: 'static',
+    favicon: './assets/images/favicon.png',
+  },
+  plugins: [
+    'expo-router',
+    [
+      'expo-splash-screen',
+      {
+        image: './assets/images/splash-icon.png',
+        imageWidth: 200,
+        resizeMode: 'contain',
+        backgroundColor: '#ffffff',
+      },
+    ],
+    'expo-font',
+    'expo-web-browser',
+  ],
+  experiments: {
+    typedRoutes: true,
+    reactCompiler: true,
+  },
+};
+
+export default config;
--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,17 @@
 const { defineConfig } = require('eslint/config');
 const expoConfig = require('eslint-config-expo/flat');
+const reactCompiler = require('eslint-plugin-react-compiler');
 
 module.exports = defineConfig([
   expoConfig,
   {
     ignores: ['dist/*'],
+    plugins: {
+      'react-compiler': reactCompiler,
+    },
     rules: {
+      'react-compiler/react-compiler': 'error',
+
       quotes: ['error', 'single', { avoidEscape: true }],
       semi: ['error', 'always'],
       'no-multiple-empty-lines': ['error', { max: 1, maxEOF: 0 }],
