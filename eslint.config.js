4ebdae6a435d24a2adfbd99b8e323e2a8cae0f9d
Felipe Krein Rocha <felipekrein@gmail.com>
2025-04-16 07:54:29 -0300

    feat: udpate expo, add eslint

---
+++ eslint.config.js
@@ -0,0 +1,28 @@
+const { defineConfig } = require('eslint/config');
+const expoConfig = require('eslint-config-expo/flat');
+
+module.exports = defineConfig([
+  expoConfig,
+  {
+    ignores: ['dist/*'],
+    rules: {
+      quotes: ['error', 'single', { avoidEscape: true }],
+      semi: ['error', 'always'],
+      'no-multiple-empty-lines': ['error', { max: 1, maxEOF: 0 }],
+      'no-trailing-spaces': 'error',
+      'comma-spacing': ['error', { before: false, after: true }],
+      'keyword-spacing': ['error', { before: true, after: true }],
+      'space-before-blocks': ['error', 'always'],
+      'space-infix-ops': 'error',
+      'block-spacing': ['error', 'always'],
+      'arrow-spacing': ['error', { before: true, after: true }],
+      'object-curly-spacing': ['error', 'always'],
+      'array-bracket-spacing': ['error', 'never'],
+      'import/order': ['error', {
+        groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+        'newlines-between': 'always',
+        alphabetize: { order: 'asc', caseInsensitive: true },
+      }],
+    },
+  }
+]);
