bdbd05ed77da4c41642f56fd330595187764d827
Felipe Krein Rocha <felipekrein@gmail.com>
2025-04-16 08:41:54 -0300

    fix: lint indent tab

--- app.config.ts
+++ app.config.ts
@@ -1,54 +1,54 @@
 import { ExpoConfig } from 'expo/config';
 
 const config: ExpoConfig = {
-  name: 'frontend-take-home',
-  slug: 'frontend-take-home',
-  version: '1.0.0',
-  orientation: 'portrait',
-  icon: './assets/images/icon.png',
-  scheme: 'myapp',
-  userInterfaceStyle: 'automatic',
-  newArchEnabled: true,
-  ios: {
-    bundleIdentifier: 'com.felipekrein.frontendtakehome',
-    supportsTablet: true,
-  },
-  android: {
-    package: 'com.felipekrein.frontendtakehome',
-    adaptiveIcon: {
-      foregroundImage: './assets/images/adaptive-icon.png',
-      backgroundColor: '#ffffff',
-    },
-  },
-  web: {
-    bundler: 'metro',
-    output: 'static',
-    favicon: './assets/images/favicon.png',
-  },
-  plugins: [
-    'expo-router',
-    'expo-dev-client',
-    [
-      'expo-splash-screen',
-      {
-        image: './assets/images/splash-icon.png',
-        imageWidth: 200,
-        resizeMode: 'contain',
-        backgroundColor: '#ffffff',
-      },
-    ],
-    'expo-font',
-    'expo-web-browser',
-  ],
-  experiments: {
-    typedRoutes: true,
-    reactCompiler: true,
-  },
-  extra: {
-    eas: {
-      projectId: '1e7f8026-f4e8-4f40-b16f-74078f426859'
-    }
-  }
+	name: 'frontend-take-home',
+	slug: 'frontend-take-home',
+	version: '1.0.0',
+	orientation: 'portrait',
+	icon: './assets/images/icon.png',
+	scheme: 'myapp',
+	userInterfaceStyle: 'automatic',
+	newArchEnabled: true,
+	ios: {
+		bundleIdentifier: 'com.felipekrein.frontendtakehome',
+		supportsTablet: true,
+	},
+	android: {
+		package: 'com.felipekrein.frontendtakehome',
+		adaptiveIcon: {
+			foregroundImage: './assets/images/adaptive-icon.png',
+			backgroundColor: '#ffffff',
+		},
+	},
+	web: {
+		bundler: 'metro',
+		output: 'static',
+		favicon: './assets/images/favicon.png',
+	},
+	plugins: [
+		'expo-router',
+		'expo-dev-client',
+		[
+			'expo-splash-screen',
+			{
+				image: './assets/images/splash-icon.png',
+				imageWidth: 200,
+				resizeMode: 'contain',
+				backgroundColor: '#ffffff',
+			},
+		],
+		'expo-font',
+		'expo-web-browser',
+	],
+	experiments: {
+		typedRoutes: true,
+		reactCompiler: true,
+	},
+	extra: {
+		eas: {
+			projectId: '1e7f8026-f4e8-4f40-b16f-74078f426859'
+		}
+	}
 };
 
 export default config;
