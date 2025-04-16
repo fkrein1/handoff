2ccd78a9fa378f1dfae13be2609738d4ecb456ad
Felipe Krein Rocha <felipekrein@gmail.com>
2025-04-16 18:06:58 -0300

    feat: section list and theming on estimate mobile screen

---
+++ src/common/lib/formatting.ts
@@ -0,0 +1,9 @@
+export const formatCurrency = (
+  amount: number,
+  locale = "en-US",
+  currency = "USD",
+) =>
+  new Intl.NumberFormat(locale, {
+    style: "currency",
+    currency,
+  }).format(amount);
--- src/estimate/EstimateScreen.tsx
+++ src/estimate/EstimateScreen.tsx
@@ -1,22 +1,31 @@
+import Entypo from "@expo/vector-icons/Entypo";
 import { BottomSheetView } from "@gorhom/bottom-sheet";
 import { useRef } from "react";
-import { View, StyleSheet, ScrollView, Pressable } from "react-native";
+import { View, Pressable, Button, SectionList } from "react-native";
 
 import type { EstimateRow, EstimateSection } from "@/data";
 
 import { BottomSheet } from "../common/components/BottomSheet";
 import { Text } from "../common/components/Text";
 import { TextField } from "../common/components/TextField";
+import { ThemeSwitch } from "../common/components/ThemeSwith";
+import { useThemeScheme } from "../common/hooks/useCurrentThemeScheme";
 import {
   calculateSectionTotal,
   calculateEstimateTotal,
 } from "../common/lib/estimate";
+import { formatCurrency } from "../common/lib/formatting";
+import { createThemedStyleSheet } from "../common/theme/themedStyles";
+import { numbersAliasTokens } from "../common/theme/tokens/alias/numbers";
+import { ThemeScheme } from "../common/theme/types";
 
 import { EditForm } from "./EditForm";
 import { useEstimateScreen } from "./useEstimateScreen";
 
 export default function EstimateScreen() {
   const bottomSheetRef = useRef<BottomSheet>(null);
+  const { theme } = useThemeScheme();
+  const styles = getStyles(theme);
 
   const {
     estimate,
@@ -46,44 +55,73 @@ export default function EstimateScreen() {
 
   return (
     <View style={styles.container}>
-      <ScrollView>
-        <TextField
-          style={styles.titleInput}
-          value={estimate.title}
-          onChangeText={updateTitle}
-          placeholder="Enter estimate title"
-        />
-        {estimate.sections.map((section) => (
-          <View key={section.id} style={styles.section}>
+      <ThemeSwitch />
+      <SectionList
+        sections={estimate.sections.map((section) => ({
+          ...section,
+          data: section.rows,
+        }))}
+        keyExtractor={(item) => item.id}
+        stickySectionHeadersEnabled
+        showsVerticalScrollIndicator={false}
+        renderSectionHeader={({ section }) => {
+          const isFirstSection = section.id === estimate.sections[0]?.id;
+          return (
             <Pressable
               onPress={() => handleSectionPress(section)}
-              style={styles.sectionHeader}
+              style={[
+                styles.sectionHeader,
+                isFirstSection && styles.firstSectionHeader,
+              ]}
             >
-              <Text>{section.title}</Text>
-              <Text>${calculateSectionTotal(section).toFixed(2)}</Text>
-            </Pressable>
-            {section.rows.map((row) => (
-              <Pressable
-                key={row.id}
-                style={styles.row}
-                onPress={() => handleItemPress(row)}
-              >
-                <View style={styles.rowLeftContent}>
-                  <Text style={styles.rowTitle}>{row.title}</Text>
-                  <Text style={styles.rowPriceDetails}>
-                    ${row.price.toFixed(2)} × {row.quantity} {row.uom}
-                  </Text>
+              <View style={styles.sectionLeftContent}>
+                <Text style={styles.sectionHeaderText}>{section.title}</Text>
+                <View style={styles.plusIconWrapper}>
+                  <Entypo
+                    style={styles.plusIcon}
+                    name="plus"
+                    size={numbersAliasTokens.sizing.icon.lg}
+                  />
                 </View>
-                <Text>${(row.price * row.quantity).toFixed(2)}</Text>
-              </Pressable>
-            ))}
+              </View>
+              <Text style={styles.sectionHeaderText}>
+                {formatCurrency(calculateSectionTotal(section))}
+              </Text>
+            </Pressable>
+          );
+        }}
+        renderItem={({ item: row }) => (
+          <Pressable style={styles.row} onPress={() => handleItemPress(row)}>
+            <View style={styles.rowLeftContent}>
+              <Text style={styles.rowTitle}>{row.title}</Text>
+              <Text style={styles.rowPriceDetails}>
+                {formatCurrency(row.price)} × {row.quantity} {row.uom}
+              </Text>
+            </View>
+            <Text style={styles.rowTitle}>
+              {formatCurrency(row.price * row.quantity)}
+            </Text>
+          </Pressable>
+        )}
+        ListHeaderComponent={
+          <TextField
+            style={styles.titleInput}
+            value={estimate.title}
+            onChangeText={updateTitle}
+            multiline
+            keyboardAppearance={theme}
+            placeholder="Enter estimate title"
+          />
+        }
+        ListFooterComponent={
+          <View style={styles.estimateTotal}>
+            <Text style={styles.estimateTotalText}>Total:</Text>
+            <Text style={styles.estimateTotalText}>
+              {formatCurrency(calculateEstimateTotal(estimate))}
+            </Text>
           </View>
-        ))}
-        <View style={styles.estimateTotal}>
-          <Text>Total:</Text>
-          <Text>${calculateEstimateTotal(estimate).toFixed(2)}</Text>
-        </View>
-      </ScrollView>
+        }
+      />
 
       <BottomSheet
         ref={bottomSheetRef}
@@ -109,56 +147,81 @@ export default function EstimateScreen() {
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#f5f5f5",
-  },
-  titleInput: {
-    fontSize: 24,
-    fontWeight: "bold",
-    padding: 16,
-    backgroundColor: "#fff",
-  },
-  section: {
-    marginBottom: 16,
-    backgroundColor: "#fff",
-  },
-  sectionHeader: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    padding: 16,
-    backgroundColor: "#f5f5f5",
-    borderBottomWidth: 1,
-    borderBottomColor: "#e0e0e0",
-  },
-  row: {
-    flexDirection: "row",
-    padding: 16,
-    borderBottomWidth: 1,
-    borderBottomColor: "#e0e0e0",
-    justifyContent: "space-between",
-    alignItems: "center",
-  },
-  rowLeftContent: {
-    flex: 1,
-    marginRight: 16,
-  },
-  rowTitle: {
-    fontSize: 16,
-    marginBottom: 4,
-    fontWeight: "500",
-  },
-  rowPriceDetails: {
-    fontSize: 14,
-  },
-  estimateTotal: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    padding: 16,
-    backgroundColor: "#fff",
-    borderTopWidth: 1,
-    borderTopColor: "#e0e0e0",
-    marginTop: 8,
-  },
-});
+export const getStyles = (theme: ThemeScheme) =>
+  createThemedStyleSheet(theme, ({ colors, numbers, fonts }) => ({
+    container: {
+      flex: 1,
+      backgroundColor: colors.layer.solid.medium,
+    },
+    titleInput: {
+      ...fonts.bold.headline.sm,
+      color: colors.text.primary,
+      paddingInline: numbers.spacing.md,
+    },
+    firstSectionHeader: {
+      borderTopWidth: numbers.outlineHeight.xs,
+    },
+    sectionHeader: {
+      backgroundColor: colors.layer.solid.medium,
+      flexDirection: "row",
+      justifyContent: "space-between",
+      alignItems: "center",
+      padding: numbers.spacing.sm,
+      borderBottomWidth: numbers.outlineHeight.xs,
+      borderColor: colors.outline.medium,
+    },
+    sectionHeaderText: {
+      ...fonts.bold.text.md,
+      backgroundColor: colors.layer.solid.medium,
+      color: colors.text.primary,
+    },
+    sectionLeftContent: {
+      flex: 1,
+      flexDirection: "row",
+      gap: numbers.spacing["3xs"],
+      alignItems: "center",
+    },
+    plusIconWrapper: {
+      borderRadius: numbers.borderRadius.pill,
+      backgroundColor: colors.layer.alpha.lightNeutral,
+      height: numbers.sizing.icon.xl,
+      width: numbers.sizing.icon.xl,
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    plusIcon: {
+      color: colors.icon.primary,
+    },
+    row: {
+      flexDirection: "row",
+      padding: numbers.spacing.sm,
+      borderBottomWidth: numbers.outlineHeight.xs,
+      borderColor: colors.outline.medium,
+      justifyContent: "space-between",
+      alignItems: "flex-start",
+      backgroundColor: colors.layer.solid.light,
+    },
+    rowLeftContent: {
+      flex: 1,
+      marginRight: numbers.spacing.sm,
+    },
+    rowTitle: {
+      ...fonts.regular.text.md,
+      color: colors.text.primary,
+      marginBottom: numbers.spacing["3xs"],
+    },
+    rowPriceDetails: {
+      ...fonts.regular.text.sm,
+      color: colors.text.secondary,
+    },
+    estimateTotal: {
+      flexDirection: "row",
+      justifyContent: "space-between",
+      padding: numbers.spacing.sm,
+      backgroundColor: colors.layer.solid.medium,
+    },
+    estimateTotalText: {
+      ...fonts.regular.text.md,
+      color: colors.text.primary,
+    },
+  }));
